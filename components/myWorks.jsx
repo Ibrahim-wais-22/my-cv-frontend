e1227997201cd285b4b8f03b5a867b829bcb1d36
ibrahim wais <67577024+Ibrahim-wais-22@users.noreply.github.com>
2022-10-20 21:52:01 -0700

    Update myWorks.jsx

--- components/myWorks.jsx
+++ components/myWorks.jsx
@@ -1,15 +1,17 @@
+import Image from 'next/image';
 function MyWorks() {
     // دالة تقوم باضهار المحتوى بعد الاسساره الى الصوره
     return (
+        
         <div className="myWorks">
-            <div className="container"><div className="card"><h2>Title the work</h2> <a href="https://ibrahim-wais-22.github.io/traning-day-6/"   ><img src="/3.png" alt="" /></a></div></div>
-            <div className="container"><div className="card"><h2>Title the work</h2> <a href="https://ibrahim-wais-22.github.io/trainig-today-5/" ><img src="/4.png" alt="" /></a></div></div>
-            <div className="container"><div className="card"><h2>Title the work</h2> <a href="https://ibrahim-wais-22.github.io/front-end/#" ><img src="/5.png" alt="" /></a></div></div>
-            <div className="container"><div className="card"><h2>Title the work</h2> <a href="https://ibrahim-wais-22.github.io/kaizon-project-level1/" ><img src="/6.png" alt="" /></a></div></div>
-            <div className="container"><div className="card"><h2>Title the work</h2> <a href="https://ibrahim-wais-22.github.io/my-app-next-js/" ><img src="/7.png" alt="" /></a></div></div>
-            <div className="container"><div className="card"><h2>Title the work</h2> <a href="https://ibrahim-wais-22.github.io/day6-js/" ><img src="/8.png" alt="" /></a></div></div>
-            <div className="container"><div className="card"><h2>Title the work</h2> <a href="https://ibrahim-wais.herokuapp.com/" ><img src="/boge site.png" alt="" /></a></div></div>
-            <div className="container"><div className="card"><h2>Title the work</h2> <a href="https://ibrahim-wais-22.github.io/trainig-today-7/" ><img src="/phone shope.png" alt="" /></a></div></div>
+            <div className="container"><div className="card"> <a href="https://ibrahim-wais.herokuapp.com/" ><Image src="/boge site.png"alt="Landscape picture" width={550} height={300}/></a></div></div>
+            <div className="container"><div className="card"> <a href="https://ibrahim-wais-22.github.io/trainig-today-7/" ><Image src="/phone shope.png"alt="Landscape picture" width={550} height={300}/></a></div></div>
+            <div className="container"><div className="card"><a href="https://ibrahim-wais-22.github.io/traning-day-6/"   ><Image src="/3.png"alt="Landscape picture" width={550} height={300}/></a></div></div>
+            <div className="container"><div className="card"> <a href="https://ibrahim-wais-22.github.io/trainig-today-5/" ><Image src="/4.png"alt="Landscape picture" width={550} height={300}/></a></div></div>
+            <div className="container"><div className="card"> <a href="https://ibrahim-wais-22.github.io/front-end/#" ><Image src="/5.png"alt="Landscape picture" width={550} height={300}/></a></div></div>
+            <div className="container"><div className="card"> <a href="https://ibrahim-wais-22.github.io/kaizon-project-level1/" ><Image src="/6.png"alt="Landscape picture" width={550} height={300}/></a></div></div>
+            <div className="container"><div className="card"> <a href="https://ibrahim-wais-22.github.io/my-app-next-js/" ><Image src="/7.png"alt="Landscape picture" width={550} height={300}/></a></div></div>
+            <div className="container"><div className="card"> <a href="https://ibrahim-wais-22.github.io/day6-js/" ><Image src="/8.png"alt="Landscape picture" width={550} height={300}/></a></div></div>
            
             
             
