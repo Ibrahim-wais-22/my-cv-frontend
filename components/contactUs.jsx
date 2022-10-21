8b3763443d908312992b7ce2cf35bc101dcfc50f
ibrahim wais <67577024+Ibrahim-wais-22@users.noreply.github.com>
2022-10-20 21:11:58 -0700

    finsh input data

--- components/contactUs.jsx
+++ components/contactUs.jsx
@@ -4,31 +4,53 @@ function ContactUs() {
             <div className="titleCertifucates">
                 <hr /> <h3>Certifucates</h3><hr />
             </div>
-            <ul>
-                <li>dasdsd</li>
-                <li>sadafV</li>
-                <li>ASDASDSD</li>
-                <li>ADADFDFAD</li>
-            </ul>
+            
+                <h2 > Bachelor`s degree in Computer Science, Shendi University</h2>
+                
             
             <div className="titleCertifucates hrSkills">
                 <hr /> <h3>skills </h3><hr />
             </div>
-            <ul>
-                <li>dasdsd</li>
-                <li>sadafV</li>
-                <li>ASDASDSD</li>
-                <li>ADADFDFAD</li>
-            </ul>
+            <div className="skillList">
+                <div className="skillList1">
+
+           <h3>HTML</h3>
+           <h3>JS</h3>
+           <h3>CSS</h3>
+           <h3>GIT</h3>
+           <h3>github</h3>
+           <h3>Reactjs</h3>
+                </div>
+                <div className="skillList2">
+
+            <h3>Nextjs</h3>
+            <h3>bootstrab</h3>
+            <h3>json</h3>
+            <h3>API</h3>
+            <h3>python</h3>
+            <h3>Django</h3>
+                </div>
+            </div>
+            {/* <img src="/logo-linkedin.svg"  className="imgContact"/> */}
             <div className="titleCertifucates ">
                 <hr /> <h3>contact my</h3><hr />
             </div>
-            <ul>
-                <li>dasdsd</li>
-                <li>sadafV</li>
-                <li>ASDASDSD</li>
-                <li>ADADFDFAD</li>
-            </ul>
+            <div className="logos">
+
+               
+                <a href="https://github.com/Ibrahim-wais-22"><img src="/logo-github.svg" alt="" className="imgContact"/></a>
+                <a href="https://www.facebook.com/ibrahim.wais.1401"><img src="/logo-facebook.svg" alt=""  className="imgContact"/></a>
+                <a href=""><img src="/logo-linkedin.svg" alt=""  className="imgContact"/></a>
+                <a href="https://twitter.com/Ibrahimibra22"><img src="/logo-twitter.svg" alt="" className="imgContact" /></a>
+                {/* <a href=""><img src="logo-instagram.svg" alt=""  className="imgContact"/></a> */}
+            </div>
+                
+                
+           
+            <hr />
+            <p className="pEmal">Email : ibrahimtur298@gmail.com</p>
+            {/* <br /> */}
+            <p>Cell phone :00967773030624</p>
             
         </div>
     )
